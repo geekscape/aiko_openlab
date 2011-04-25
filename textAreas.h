5a9036da5d059cf4bc67417cf3561408065243c6
Paul Zee <paulszym+github@gmail.com>
2011-04-26 01:48:08 +1000

    Add other screen text areas to enumerated list:  - help text area  - screen title area

--- textAreas.h
+++ textAreas.h
@@ -7,28 +7,37 @@ typedef enum  {
 	/**<Upper GLCD display area */
 	textAreaGRAPH = MK_TareaToken(0, 0, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 10),
 
-	/**<Left-hand POT Label Border*/
+	/**<Screen Title */
+	textAreaSCRTTLBDR = MK_TareaToken(0, 0, DISPLAY_WIDTH - 1, 9),
+
+	/**<Screen Title Border*/
+	textAreaSCRTTL = MK_TareaToken(1, 1, DISPLAY_WIDTH - 1, 8),
+
+	/**<Screen Instructions area */
+	textAreaHELP = MK_TareaToken(0, 10, DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 10),
+        
+	/**<Left-hand POT Label Border */
 	textAreaPOTLBLBDR1 = MK_TareaToken(0, DISPLAY_HEIGHT - 9, 18, DISPLAY_HEIGHT),
 
 	/**<Left-hand POT Label */
 	textAreaPOTLBL1 = MK_TareaToken(1, DISPLAY_HEIGHT - 8, 18, DISPLAY_HEIGHT-1),
 
-	/**<Left-hand POT Label Border*/
+	/**<Left-hand POT value Border */
 	textAreaPOTVALBDR1 = MK_TareaToken(19, DISPLAY_HEIGHT-9, 63, DISPLAY_HEIGHT),
 
-	/**<Left-hand POT Label */
+	/**<Left-hand POT Value Display */
 	textAreaPOTVAL1 = MK_TareaToken(21, DISPLAY_HEIGHT - 8, 63, DISPLAY_HEIGHT-1),
 
-	/**<Left-hand POT Label Border*/
+	/**<Right-hand POT Label Border */
 	textAreaPOTLBLBDR2 = MK_TareaToken(64, DISPLAY_HEIGHT-9, 82, DISPLAY_HEIGHT),
 
 	/**<Right-hand POT Label */
 	textAreaPOTLBL2 = MK_TareaToken(65, DISPLAY_HEIGHT - 8, 82, DISPLAY_HEIGHT-1),
 
-	/**<Left-hand POT Label Border*/
+	/**<Right-hand POT Value Border */
 	textAreaPOTVALBDR2 = MK_TareaToken(83, DISPLAY_HEIGHT-9, 127, DISPLAY_HEIGHT),
 
-	/**<Right-hand POT Label */
+	/**<Right-hand POT Value Display */
 	textAreaPOTVAL2 = MK_TareaToken(85, DISPLAY_HEIGHT-8, 127, DISPLAY_HEIGHT-1),
 }
   predefinedLABArea;
